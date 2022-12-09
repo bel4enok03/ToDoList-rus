fa19dfd8018251765e1f4012b1c4d0cc6dab5408
bel4enok03 <belskaya-tanyush@mail.ru>
2022-12-09 16:48:39 +0400

    *

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-const inputBox = document.querySelector ('.inputBox');
-const btn = document.querySelector ('.btn');
-const toDoContainer = document.querySelector ('.toDoContainer');
+const inputBox = document.querySelector('.inputBox');
+const btn = document.querySelector('.btn');
+const toDoContainer = document.querySelector('.toDoContainer');
 
-btn.addEventListener ('click',function() {
+btn.addEventListener('click', function () {
+	const item = document.createElement('li');
+	item.innerText = inputBox.value;
+	item.classList.add('toDoAdd');
+	toDoContainer.appendChild(item);
+	inputBox.value = '';
 
-    const item = document.createElement ('li');
-    item.innerText = inputBox.value; 
-    item.classList.add('toDoAdd')
-    toDoContainer.appendChild(item);
-    inputBox.value = '';
+	item.addEventListener('click', function () {
+		item.classList.add('toDoCompleted');
+	});
+	item.addEventListener('dblclick', function () {
+		toDoContainer.removeChild(item);
+	});
+});
 
-    item.addEventListener ('click', function() {
-        item.classList.add('toDoCompleted')
-    })
-    item.addEventListener ('dblclick', function() {
-        toDoContainer.removeChild(item);
-    })
-})
+gsap.to('h1', {
+	text: 'Мой список дел на день',
+	duration: 1.2,
+    delay:1,
+	ease: 'power1.in',
+    
+});
+
+gsap.to('.container', {
+	duration: 1,
+	delay: 3,
+	y: 100,
+    opacity:1
+});
+
+gsap.to('.toDoContainer', {
+	duration: 1,
+	delay: 2,
+	y: 120,
+    opacity:1
+});
